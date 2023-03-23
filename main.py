3d0ede9559dc73a6c8d3bf91757c6473b4e91b8b
Giko_20 <abesadzegivi9@gmail.com>
2023-03-23 17:33:21 +0400

    adding main code

---
+++ main.py
@@ -0,0 +1,94 @@
+# import libraries
+import sqlite3
+import os
+import subprocess
+import random
+import string
+
+# if name is not presented this function generates random name
+def get_random_string(length=8):
+    # choose from all lowercase letter
+    letters = string.ascii_lowercase
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
+
+# input the name of database
+database_name = input("Enter SqLite Database name to create connection: ")
+if database_name != '':
+    database = database_name
+else:
+    database = get_random_string()
+
+# connect to SqLite database
+conn = sqlite3.connect(f'{database}.db')
+print('Succesfully connected!')
+c = conn.cursor()
+
+# function to execute sql queries
+def execute_query(query):
+    result = c.execute(query)
+    return result.fetchall()
+
+# function to display results
+def display_results(results):
+    for row in results:
+        print(row)
+
+# allowing user to run commands
+while True:
+        command = input("Enter a Command (or 'quit' to exit): ")
+        if command == 'quit':
+            break
+        else:
+            # executing SQL queries
+            if command.endswith(';'):
+                try:
+                    results = execute_query(command)
+                    display_results(results)
+                except sqlite3.Error as e:
+                    print('Error executing query:', e)
+
+                if command.startswith('SELECT') or command.startswith('select'):
+                    file_response = input("Do you want to store query results into the file?(y/n): ")
+                    if file_response == 'y':
+                        filename = input("Enter a filename to save the results: ")
+                        if filename != '':
+                            file = filename
+                        else:
+                            file = get_random_string()
+
+                        with open(file, 'w') as f:
+                            for row in results:
+                                f.write(str(row) + '\n')
+                        print('result saved successfully!')
+
+                        # ask if user wants to make manipulations
+                        file_manipulates = input('Do you want to make manipulations on this file? (y/n): ')
+
+                        if file_manipulates == 'y':
+                            py_script = input('Enter a Python script to make manipulations on this file: ')
+
+                            # Check if the user entered a Python script to execute
+                            if py_script.endswith('.py'):
+                                try:
+                                    # Execute the script using the subprocess module
+                                    subprocess.run(['python', py_script, file])
+                                except Exception as sub_error:
+                                    print('Error during manipulation ', sub_error)
+                        else:
+                            pass
+
+                    else: pass
+
+            # to run any python script
+            elif command.endswith('.py'):
+                subprocess.run(['python', command])
+
+            # to run linux commands
+            else:
+                os.system(command)
+
+# commit executed commands and close database connection
+conn.commit()
+conn.close()
+
